efad72f769801978c49d6f96cf8189f4cd054d1d
CallmeDwi <117493103+CallmeDwi@users.noreply.github.com>
2024-07-20 21:07:48 +0700

    logic cubit

--- lib/cubit/app_cubit_logics.dart
+++ lib/cubit/app_cubit_logics.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:perhutaniwisata_app/cubit/app_cubit.dart';
 import 'package:perhutaniwisata_app/cubit/app_cubit_states.dart';
+import 'package:perhutaniwisata_app/pages/detail_pages/detail_page.dart';
 import 'package:perhutaniwisata_app/pages/welcome_page.dart';
 import 'package:perhutaniwisata_app/pages/home_page.dart'; // Sesuaikan dengan path Anda
 import 'package:perhutaniwisata_app/pages/profile_pages/login_page.dart'; // Sesuaikan dengan path Anda
@@ -22,6 +23,8 @@ class _AppCubitLogicsState extends State<AppCubitLogics> {
           return HomePage();
         } else if (state is LoginPageState) {
           return LoginPage();
+        } else if (state is DetailState) {
+          return DetailPage();
         } else {
           return Scaffold(
             body: Center(
