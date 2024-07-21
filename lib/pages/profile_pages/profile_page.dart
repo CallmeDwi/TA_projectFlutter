52c0def4ccfb97f1f72719817ff8cc770d8d0a11
CallmeDwi <117493103+CallmeDwi@users.noreply.github.com>
2024-07-22 00:46:01 +0700

    profil page terhubung ke firebase

--- lib/pages/profile_pages/profile_page.dart
+++ lib/pages/profile_pages/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:perhutaniwisata_app/components/text_box.dart';
@@ -11,65 +12,119 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   final currentUser = FirebaseAuth.instance.currentUser!;
-  Future<void> editField(String field) async {}
+  final usersCollection = FirebaseFirestore.instance.collection('Users');
+
+  Future<void> editField(String field) async {
+    TextEditingController controller = TextEditingController();
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Edit $field"),
+        content: TextField(
+          controller: controller,
+          decoration: InputDecoration(hintText: "Masukkan $field baru"),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Batal'),
+          ),
+          TextButton(
+            onPressed: () async {
+              if (controller.text.isNotEmpty) {
+                await usersCollection
+                    .doc(currentUser.uid) // Menggunakan uid sebagai ID dokumen
+                    .update({field: controller.text});
+              }
+              Navigator.of(context).pop();
+            },
+            child: Text('Simpan'),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile Page'),
+        title: Text('Profil'),
         centerTitle: true,
         backgroundColor: Colors.grey[900],
       ),
-      body: ListView(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          const Icon(
-            Icons.person,
-            size: 72,
-          ),
-          Text(
-            currentUser.email!,
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.grey[700]),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 25),
-            child: Text(
-              'My Details',
-              style: TextStyle(
-                color: Colors.grey[600],
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: usersCollection
+            .doc(currentUser.uid) // Menggunakan uid sebagai ID dokumen
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || !snapshot.data!.exists) {
+            return Center(child: Text('Data tidak ditemukan.'));
+          }
+
+          final userData = snapshot.data!.data() as Map<String, dynamic>?;
+
+          if (userData == null) {
+            return Center(child: Text('Data tidak ditemukan.'));
+          }
+
+          return ListView(
+            children: [
+              const SizedBox(
+                height: 50,
               ),
-            ),
-          ),
-          MyTextBox(
-            text: 'ojan',
-            sectionName: 'ussername',
-            onPressed: () => editField('username'),
-          ),
-          MyTextBox(
-            text: 'empty bio',
-            sectionName: 'bio',
-            onPressed: () => editField('bio'),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 25),
-            child: Text(
-              'My Favorite',
-              style: TextStyle(
-                color: Colors.grey[600],
+              const Icon(
+                Icons.person,
+                size: 72,
               ),
-            ),
-          ),
-        ],
+              Text(
+                currentUser.email!,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey[700]),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: Text(
+                  'Detail Saya',
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ),
+              MyTextBox(
+                text: userData['username'] ?? 'Tidak ada username',
+                sectionName: 'Username',
+                onPressed: () => editField('username'),
+              ),
+              MyTextBox(
+                text: userData['bio'] ?? 'Tidak ada bio',
+                sectionName: 'Bio',
+                onPressed: () => editField('bio'),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: Text(
+                  'Favorit Saya',
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
